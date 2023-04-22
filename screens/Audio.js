1cfa56879787e42ebd09584f69d036126a90321f
LizCarmen <117939790+LizC97@users.noreply.github.com>
2023-04-22 16:33:03 +0200

    camara y audio

--- screens/Audio.js
+++ screens/Audio.js
@@ -1,12 +1,50 @@
-import React from 'react';
-import { StyleSheet, Text, TextInput, View } from 'react-native';
+import * as React from 'react';
+import { Text, View, StyleSheet, Button, Alert } from 'react-native';
+import { Audio } from 'expo-av';
 
+export default function App() {
+  const [recording, setRecording] = React.useState();
 
-export default function Audio({ navigation }) {
+  async function startRecording() {
+    try {
+      console.log('Requesting permissions..');
+      await Audio.requestPermissionsAsync();
+      await Audio.setAudioModeAsync({
+        allowsRecordingIOS: true,
+        playsInSilentModeIOS: true,
+      });
+
+      console.log('Starting recording..');
+      const { recording } = await Audio.Recording.createAsync(
+        Audio.RecordingOptionsPresets.HIGH_QUALITY
+      );
+      setRecording(recording);
+      console.log('Recording started');
+    } catch (err) {
+      console.error('Failed to start recording', err);
+    }
+  }
+
+  async function stopRecording() {
+    console.log('Stopping recording..');
+    setRecording(undefined);
+    await recording.stopAndUnloadAsync();
+    await Audio.setAudioModeAsync({
+      allowsRecordingIOS: false,
+    });
+    const uri = recording.getURI();
+    console.log('Recording stopped and stored at', uri);
+    Alert.alert('Audio grabado', `El audio se ha guardado en: ${uri}`, [
+      { text: 'OK', onPress: () => console.log('OK Pressed') },
+    ]);
+  }
 
   return (
     <View style={styles.container}>
-      <Text style={styles.title}>Audio</Text>
+      <Button
+        title={recording ? 'Stop Recording' : 'Start Recording'}
+        onPress={recording ? stopRecording : startRecording}
+      />
     </View>
   );
 }
@@ -14,15 +52,8 @@ export default function Audio({ navigation }) {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    justifyContent: 'flex-start',
+    justifyContent: 'center',
     alignItems: 'center',
-    paddingHorizontal: 20,
     backgroundColor: '#fff',
-    paddingTop: 100
   },
-  title: {
-    fontSize: 24,
-    fontWeight: 'bold',
-    marginBottom: 20,
-  }
 });
--- screens/Camara.js
+++ screens/Camara.js
@@ -1,28 +1,150 @@
-import { React, useState } from 'react';
-import { View, Text, StyleSheet } from 'react-native';
+import React, { useState, useEffect } from "react";
+import { Alert } from 'react-native';
+import {
+  Button,
+  Platform,
+  StatusBar,
+  StyleSheet,
+  Text,
+  View,
+} from "react-native";
+import { Camera } from "expo-camera";
+import * as MediaLibrary from "expo-media-library";
+import * as Permissions from "expo-permissions";
+import * as ImagePicker from "expo-image-picker";
 
-export default function Camara() {
+const App = () => {
+  const [hasCameraPermission, setHasCameraPermission] = useState(null);
+  const [cameraType, setCameraType] = useState(Camera.Constants.Type.back);
+  const [cameraRef, setCameraRef] = useState(null);
+  const [userName, setUserName] = useState("Usuario");
 
-  return (
-    <View style={styles.container}>
-      <Text style={styles.title}>Cámara</Text>
-    </View>
-  );
-}
+  useEffect(() => {
+    getCameraPermission();
+  }, []);
+
+  const getCameraPermission = async () => {
+    const { status } = await Camera.requestCameraPermissionsAsync();
+    setHasCameraPermission(status === "granted");
+  };
+
+  const getMediaLibraryPermission = async () => {
+    try {
+      const { status } = await MediaLibrary.requestPermissionsAsync();
+      if (status === "granted") {
+        // Obtener las fotos y videos creados con la aplicación
+        const { assets } = await MediaLibrary.getAssetsAsync({ mediaType: [MediaLibrary.MediaType.photo, MediaLibrary.MediaType.video], createdAfter: Date.now() - 10000 });
+        return assets;
+      } else {
+        Alert.alert(
+          "Permisos requeridos",
+          "Por favor, otorga permisos de acceso a la biblioteca de medios para usar esta función.",
+          [
+            { text: "OK", onPress: () => {} }
+          ]
+        );
+        return null;
+      }
+    } catch (error) {
+      console.error("Error al solicitar permiso de la biblioteca de medios:", error);
+      return null;
+    }
+  };
+
+  const takePhoto = async () => {
+    if (cameraRef) {
+      try {
+        const photo = await cameraRef.takePictureAsync();
+        const hasPermission = await getMediaLibraryPermission();
+        if (hasPermission) {
+          await MediaLibrary.createAssetAsync(photo.uri);
+          console.log("Photo saved to gallery:", photo.uri);
+        } else {
+          console.log("No access to media library");
+        }
+      } catch (err) {
+        console.error("Error taking photo:", err);
+      }
+    }
+  };
+
+  const switchCameraType = () => {
+    setCameraType(
+      cameraType === Camera.Constants.Type.back
+        ? Camera.Constants.Type.front
+        : Camera.Constants.Type.back
+    );
+  };
+
+  const openMediaLibrary = async () => {
+    const hasPermission = await getMediaLibraryPermission();
+    if (hasPermission) {
+      const assets = await MediaLibrary.getAssetsAsync({
+        mediaType: "photo",
+        source: "user",
+      });
+      if (assets.totalCount > 0) {
+        MediaLibrary.openPickerAsync({
+          mediaType: "photo",
+          source: "user",
+        });
+      } else {
+        console.log("No photos found in media library");
+      }
+    } else {
+      console.log("No access to media library");
+    }
+  };
+
+  if (hasCameraPermission === null) {
+    return <View />;
+  } else if (hasCameraPermission === false) {
+    return <Text>No access to camera</Text>;
+  } else {
+    return (
+      <View style={styles.container}>
+        <Text style={styles.userNameText}>Hola, {userName}</Text>
+        <Camera
+          style={styles.camera}
+          type={cameraType}
+          ref={(ref) => setCameraRef(ref)}
+        >
+          <View style={styles.buttonContainer}>
+            <Button title="Switch Camera" onPress={switchCameraType} />
+            <Button title="Take Photo" onPress={takePhoto} />
+            <Button title="View Photos" onPress={openMediaLibrary} />
+          </View>
+        </Camera>
+      </View>
+    );
+  }
+};
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    justifyContent: 'flex-start',
-    alignItems: 'center',
-    paddingHorizontal: 20,
-    backgroundColor: '#fff',
-    paddingTop: 100
+    justifyContent: "center",
+    alignItems: "center",
+    backgroundColor: "black",
   },
-  title: {
+  camera: {
+    flex: 1,
+    width: "100%",
+    justifyContent: "flex-end",
+    alignItems: "center",
+  },
+  buttonContainer: {
+    flexDirection: "row",
+    justifyContent: "space-between",
+    marginBottom: 36,
+    paddingHorizontal: 20,
+    width: "100%",
+  },
+  userNameText: {
     fontSize: 24,
-    fontWeight: 'bold',
-    marginBottom: 20,
-  }
-
+    color: "white",
+    marginBottom: 16,
+  },
 });
+
+export default App;
